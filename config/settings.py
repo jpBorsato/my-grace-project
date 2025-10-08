a37b50aa8c8b8870c260c9171a62099354f14f01
jpBorsato <48165246+jpBorsato@users.noreply.github.com>
2025-10-08 12:44:44 +0000

    set `STORAGE` setting to default config

--- config/settings.py
+++ config/settings.py
@@ -91,6 +91,9 @@ DATABASES = {
 }
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
